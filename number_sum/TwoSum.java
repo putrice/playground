197e67b6222ba9971c6da0972ab7f4b4cc02883e
Avinta Putri Agistasari <putriagistasari@gmail.com>
2023-02-03 21:20:04 +0700

    Add Parent Number Sum
    Add Two Number Sum

---
+++ number_sum/TwoSum.java
@@ -0,0 +1,45 @@
+package number_sum;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    private boolean check(int[] arr, int toCheckValue)
+    {
+        Arrays.sort(arr);
+
+        int res = Arrays.binarySearch(arr, toCheckValue);
+
+        return res >= 0;
+    }
+
+    private int[] twoNumberSum(int[] array, int target) {
+        int[] results = new int[2];
+
+        for (int i = 0; i < array.length; i++) {
+            if (check(results, array[i])) {
+                continue;
+            }
+
+            int numTwo = target - array[i];
+
+            if (check(Arrays.copyOfRange(array, i, array.length), numTwo)){
+                results[0] = array[i];
+                results[1] = numTwo;
+            }
+        }
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+
+        int[] test1 = {3, 5, -4, 8, 11, 1, -1, 6};
+        int[] result = twoSum.twoNumberSum(test1, 10);
+
+        for (int j : result) {
+            System.out.println(j);
+        }
+    }
+}
