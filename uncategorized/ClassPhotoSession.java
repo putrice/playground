7d66a7a8be779246d356887383922fcfa952a10c
Avinta Putri Agistasari <putriagistasari@gmail.com>
2023-02-11 19:56:23 +0700

    Start working on Class Photo Session Case
    Init class

---
+++ uncategorized/ClassPhotoSession.java
@@ -0,0 +1,11 @@
+package uncategorized;
+
+import java.util.ArrayList;
+
+public class ClassPhotoSession {
+
+    private boolean classPhotos(
+            ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
+        return false;
+    }
+}
