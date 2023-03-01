a428a4007c62106c7f8b1f66eda46c6cd024a463
Avinta Putri Agistasari <putriagistasari@gmail.com>
2023-03-01 21:39:26 +0700

    Auto stash before rebase of "origin/master"

--- uncategorized/ClassPhotoSession.java
+++ uncategorized/ClassPhotoSession.java
@@ -7,7 +7,10 @@ public class ClassPhotoSession {
 
     private boolean classPhotos(
             ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
+        Collections.sort(redShirtHeights, Collections.reverseOrder());
+        Collections.sort(blueShirtHeights, Collections.reverseOrder());
 
+        String shirtColorInFirstRow = (redShirtHeights.get(0) < blueShirtHeights.get(0)) ? "Red" : "Blue";
         return false;
     }
 }
